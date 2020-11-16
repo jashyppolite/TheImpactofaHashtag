a4a481a51434d26a7f5550a4cda61c075ee45851
Jashyppolite <jasminehyppolite@college.harvard.edu>
2020-11-16 15:25:39 -0500

    more edits to the site, two added pages, and shifts in datasets such that they may be interacted with

--- app.R
+++ app.R
@@ -10,39 +10,51 @@
 library(shiny)
 library(readxl)
 library(tidyverse)
-#exclusive_terms_100 <- readRDS("exclusive_terms_100.RDS")
-# engagement_terms_100 <- read_xlsx("clean_engagement.xlsx")
-# exclusive_terms_100 <- read_xlsx("clean_exclusion.xlsx")
 blm_google <- readRDS("blmgoogle.RDS")
 blm_votes <- readRDS("blmvotes.RDS")
 votes_clean <- readRDS("votesclean.RDS")
+
 # Define UI for application that draws a histogram
+
 ui <- navbarPage(
   "Trends of Social Movements and Bills Concerning Similar Issues",
-  tabPanel("Model",
+  tabPanel("Bills in NY",
            fluidPage(
-             titlePanel("Google Trends of Social Movements and Bills in NY"),
+             titlePanel("Trends of Social Movements and Bills in NY"),
              sidebarLayout(
                sidebarPanel(
                  selectInput("select",
                              "Bill Type: ",
-                             choices = c("police_bill_prop", "hara_bill_prop", "assault_bill_prop")),
-                 # radioButtons(
-                 #   inputId = "wage_type",
-                 #   label = " ",
-                 #   choices = list("adj" = "state_minimum_wage.adj", "non_adj" = "state_minimum_wage")
-                 # )
+                             choices = c("police_bill_prop", "hara_bill_prop",
+                                         "assault_bill_prop")),
                ),
                mainPanel(plotOutput("billplot")))
            )),
   tabPanel("Google Trends",
-           titlePanel("Discussion Title"),
+           titlePanel("Google Trend Hashtag Data over Time"),
+           fluidPage(
+             titlePanel("Social Movements Popularity"),
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("Trend",
+                             "Hashtag",
+                             choices = c("blm", "metoo")),
+               ),
+               mainPanel(plotOutput("GooglePlot"))),
+             p("Note that theBlack Lives Matter Movement began to gain 
+               traction in 2013 after the murder of Trayvon Martin. The 
+               Me Too movement began to gain traction over social media
+               after Alyssa Milano used the hashtag in a tweet in 
+               October of 2017."),
+           )),
+  tabPanel("Google Trends and Bill Proportions",
+           titlePanel("Google Trend Hashtag Data over Time"),
            fluidPage(
              titlePanel("Google Trends of Social Movements and Bills in NY"),
              sidebarLayout(
                sidebarPanel(
-                 selectInput("select",
-                             "Bill Type: ",
+                 selectInput("bill",
+                             "Bill Type",
                              choices = c("police_bill_prop", "hara_bill_prop", "assault_bill_prop")),
                  # radioButtons(
                  #   inputId = "wage_type",
@@ -50,12 +62,24 @@ ui <- navbarPage(
                  #   choices = list("adj" = "state_minimum_wage.adj", "non_adj" = "state_minimum_wage")
                  # )
                ),
-               mainPanel(plotOutput("billplot")))
+               mainPanel(plotOutput("Comb_Plot"))),
+             p("Note that the harassment and asssault bills are to be interpreted 
+                 the #MeToo Movement in mind and police bills with the 
+                 #BlackLivesMatter Movement"),
            )),
   tabPanel("Discussion",
-           titlePanel("Discussion Title"),
+           titlePanel("Do Hashtags Really Have an Impact on Legislation?"),
            p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
+              an explanation of why you made them. Within this project I tried 
+             to compare legislation in the state of NY and hashtag patters (via
+             google trends data) to find correlation between the two phenomena. 
+             While correlation does not imply causation, I wanted to see if public
+             opinion or upset about something could have any influence on the 
+             the bills we see passed. The trends shown on the Google Trends and Bill
+             Proportions page look to answer this question. For the Black LIVes
+             Matter plot, one can see a bit of correlation while looking at the 
+             slope of the line, but there is lots of room for error as also 
+             illustrated in the plot. CONTINUE FOR OTHER PLOT")),
   tabPanel("About", 
            titlePanel("About"),
            h3("Project Background and Motivations"),
@@ -72,17 +96,6 @@ ui <- navbarPage(
            of the TechScience Program. You can reach me at
              jasminehyppolite@college.harvard.edu.")))
 
-# impact of social media 
-# when it comes to altering legislation or influencing policy makers.
-# Specifically, I wanted to look at the difference between advocacy through
-# social media in the form of a hashtag versus policy change, passing of bills,
-# etc. To do this, I look at two datasets, one consisting of all tweets and 
-# facebook post between 2015 and 2020 posted by accounts managed by 
-# any member of the US House of Representatives or Senate. The 
-# collection of tweets spans from January 1, 2015 to May 31, 2020 and sums 
-# to over 3 million tweets and 1,362 Facebook posts. This will be paired 
-
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   output$billplot <- renderPlot({
     
@@ -90,11 +103,36 @@ server <- function(input, output) {
       filter(bill_type == input$select) %>%
       ggplot(aes(x = month_year, y = bill_prop)) + 
       geom_point(color = "deepskyblue3") + 
-      theme(axis.text.x = element_text(angle = - 90, vjust = 0.5)) 
-    # + geom_smooth(method = "lm")
+      theme(axis.text.x = element_text(angle = -90, vjust = 0.5)) + 
+      labs(x = "Month and Year",
+           y = "Proportion of Bills Relating to Topic")
   })
   
-}
+  output$GooglePlot <- renderPlot({
+    
+    trends %>%
+      filter(ID == input$Trend) %>%
+      ggplot(aes(x = month_year, y = mean_score)) + 
+      geom_point(color = "red") + 
+      geom_line(color = "gray") + 
+      theme(axis.text.x = element_text(angle = - 90, vjust = 0.5)) + 
+      labs(title = "Google Trends for Frequency of # Searches by Month",
+           x = "Month and Year",
+           y = "Google Score")
+  })
+  
+  output$Comb_Plot <- renderPlot({
+    
+    trends_w_vote %>%
+      filter(bill_type == input$bill) %>%
+      ggplot(aes(x = mean_score, y = bill_prop)) + geom_point() + 
+      geom_smooth(method = "lm") + 
+      labs(title = "Correlation Between Bills in NY Regarding Bill Type and Google Trend Scores",
+           x = "Mean Google Trend Score for Movement",
+           y = "Proportion of Bills per Week")
 
+})
+
+}
 # Run the application 
 shinyApp(ui = ui, server = server)
